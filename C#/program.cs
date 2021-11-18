36080c2795d95da6e8b2f94f74c5da0aedc2d56c
Dora Bao <91505880+dorabao@users.noreply.github.com>
2021-11-17 18:28:58 -0800

    Create program.cs

---
+++ C#/program.cs
@@ -0,0 +1,33 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 1 };
+            int[] result = CAssignment.ArrExpander(nums);
+            Array.ForEach(result, Console.WriteLine);
+        }
+    }
+
+    class CAssignment
+    {
+        public static int[] ArrExpander(int[] array)
+        {
+            if (array == null) return array;
+
+            int expander = 2;
+            int length = array.Length;
+            int newLength = expander * length;
+
+            int[] newArray = new int[newLength];
+            for (int i = 0; i < newLength; i++)
+            {
+                newArray[i] = array[i % length];
+            }
+            return newArray;
+        }
+    }
+}
