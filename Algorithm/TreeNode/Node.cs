5624d770ac4aad4283452ae37a5ae36295358320
dorabao <dorriebao@gmail.com>
2021-12-12 11:09:05 -0800

    Added BFS DFS Algorithm

---
+++ Algorithm/TreeNode/Node.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections.Generic;
+
+namespace ConsoleApp2
+{
+    public class Node
+    {
+        public string Name { get; }
+
+        public List<Node> Children { get; } = new List<Node>();
+
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public List<string> DepthFirstSearch()
+        {
+            var names = new List<string>();
+            DepthFirstSearch(this, names);
+            return names;
+        }
+
+        private void DepthFirstSearch(Node root, List<string> names)
+        {
+            if (root == null) return;
+
+            names.Add(root.Name);
+            foreach (var childNode in root.Children)
+            {
+                DepthFirstSearch(childNode, names);
+            }
+        }
+
+        public List<string> BreadthFirstSearch()
+        {
+            var names = new List<string>();
+            var queue = new Queue<Node>();
+            
+            queue.Enqueue(this);
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+                names.Add(node.Name);
+
+                foreach (var childNode in node.Children)
+                {
+                    queue.Enqueue(childNode);
+                }
+            }
+
+            return names;
+        }
+
+        public Node AddChild(string name)
+        {
+            Node child = new Node(name);
+            Children.Add(child);
+            return this;
+        }
+    }
+}
---
+++ Algorithm/TreeNode/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var root = new Node("A");
+            root.AddChild("B").AddChild("C").AddChild("D");
+
+            var nodeB = root.Children[0];
+            nodeB.AddChild("E").AddChild("F");
+            
+            var nodeD = root.Children[2];
+            nodeD.AddChild("G").AddChild("H");
+
+            var nodeF = nodeB.Children[1];
+            nodeF.AddChild("I").AddChild("J");
+
+            var nodeG = nodeD.Children[0];
+            nodeG.AddChild("K");
+
+            Console.Write(string.Join(", ", root.BreadthFirstSearch()));
+            Console.WriteLine(string.Join(", ", root.DepthFirstSearch()));
+        }
+    }
+}
